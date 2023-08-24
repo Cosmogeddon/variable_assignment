743bd69ad91d4ec65d5675c6b7229bb78432f80b
Cody Kelderman <codykelderman@gmail.com>
2023-08-23 22:10:44 -0600

    finish program

--- use_constants.py
+++ use_constants.py
@@ -1,3 +1,30 @@
+
+
+
+# get variable value from constant.py (this proved troublesome with simply the 'import' command)
 from constant import PRICE_OF_HATS
 
+# define variables
+quantity = 2
+item = "hat"
+size = 7.0
+
+
+# display in terminal
+print(type(quantity))
+print(type(item))
+print(type(size))
 print(PRICE_OF_HATS)
+
+# takes variable data and prints a string which changes upon different entries
+def first_program(amount, type, bigness):
+    if amount == 1:
+        print(str(amount) + " " + type + " size " + str(bigness) + " costs $" + str(PRICE_OF_HATS))
+    elif amount == 0:
+        print("No hats for you")
+    elif amount < 0:
+        print("I don't owe you anything")
+    else:
+        print(str(amount) + " " + type + "s size " + str(bigness) + " cost $" + str((PRICE_OF_HATS)* amount))
+
+first_program(quantity, item, size)
