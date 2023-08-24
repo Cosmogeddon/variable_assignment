22b7ad70c9963e539066858eeb339f995fcd1185
Cody Kelderman <codykelderman@gmail.com>
2023-08-23 23:05:35 -0600

    start casting assignment

---
+++ arithmetic.py
@@ -0,0 +1,26 @@
+def add(num1, num2):
+    print(num1 + num2)
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+def raised(num1, num2):
+    print(num1 ** num2)
+
+def divide(num1, num2):
+    print(num1 / num2)
+
+def modulus(num1, num2):
+    print(num1 % num2)
+
+def floor_divide(num1, num2):
+    print(num1 // num2)
+
+def order_test(num1, num2, num3, num4, num5):
+    print((num1 / num2) + (num2 // num4 + num1) ** num3 / num5 - num2 ** num3 ** num2)
+
+
+floor_divide(5,-2)
